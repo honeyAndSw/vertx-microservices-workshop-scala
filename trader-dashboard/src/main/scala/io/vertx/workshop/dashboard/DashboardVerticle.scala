5f419b3070ae1b2f57fba60832606c11f9fdbb62
honey.and.sw <honey.and.sw@gmail.com>
2016-11-09 00:26:38 +0900

    Call Audit services (not available so far)

--- trader-dashboard/src/main/scala/io/vertx/workshop/dashboard/DashboardVerticle.scala
+++ trader-dashboard/src/main/scala/io/vertx/workshop/dashboard/DashboardVerticle.scala
@@ -1,7 +1,9 @@
 package io.vertx.workshop.dashboard
 
 import io.vertx.ext.web.handler.StaticHandler
-import io.vertx.scala.ext.web.Router
+import io.vertx.lang.scala.json.Json
+import io.vertx.scala.core.http.HttpClient
+import io.vertx.scala.ext.web.{Router, RoutingContext}
 import io.vertx.workshop.common.MicroServiceVerticle
 
 /**
@@ -9,10 +11,15 @@ import io.vertx.workshop.common.MicroServiceVerticle
   */
 class DashboardVerticle extends MicroServiceVerticle {
 
+  var client: HttpClient = null
+
   override def start(): Unit = {
     super.start()
 
     val router = Router.router(vertx)
+
+    // Last operations
+    router.get("/operations").handler(callAuditService)
 
     // Static content
     router.route("/*").asJava.handler(StaticHandler.create())
@@ -20,5 +27,27 @@ class DashboardVerticle extends MicroServiceVerticle {
     vertx.createHttpServer()
       .requestHandler(router.accept _)
       .listen(8080)
+  }
+
+  private def callAuditService(context: RoutingContext): Unit = {
+    if (client == null) {
+      context.response()
+        .putHeader("content-type", "application/json")
+        .setStatusCode(200)
+        .end(Json.obj(("message", "No audit service")).encode())
+    } else {
+      client.get("/", response => {
+        response
+          .exceptionHandler(context.fail _)
+          .bodyHandler(buffer => {
+            context.response()
+              .putHeader("content-type", "application/json")
+              .setStatusCode(200)
+              .end(buffer)
+        })
+      }).exceptionHandler(context.fail _)
+        .setTimeout(5000)
+        .end()
+    }
   }
 }
