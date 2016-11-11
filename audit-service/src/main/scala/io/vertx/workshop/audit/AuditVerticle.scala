a1a3ef4341e29f9322d5b7d331e8da040b1d86a9
honey.and.sw <honey.and.sw@gmail.com>
2016-11-11 22:10:38 +0900

    AuditService - composite futures

--- audit-service/src/main/scala/io/vertx/workshop/audit/AuditVerticle.scala
+++ audit-service/src/main/scala/io/vertx/workshop/audit/AuditVerticle.scala
@@ -1,17 +1,40 @@
 package io.vertx.workshop.audit
 
-import io.vertx.lang.scala.json.{JsonObject}
+import io.vertx.lang.scala.json.JsonObject
 import io.vertx.scala.core.eventbus.MessageConsumer
 import io.vertx.scala.core.http.HttpServer
 import io.vertx.workshop.common.MicroServiceVerticle
 
 import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Success}
 
 /**
   * Created by honey.and.sw on 2016. 11. 10.
   */
 class AuditVerticle extends MicroServiceVerticle {
   override def start(startPromise: Promise[Unit]) = {
+
+    val databaseReady: Future[Unit] = initializeDatabase(true)
+    var httpEndpointReady: Future[HttpServer] = configureTheHTTPServer()
+    var messageListenerReady: Future[MessageConsumer[JsonObject]] = retrieveThePortfolioMessageSource()
+
+    val ready: Future[MessageConsumer[JsonObject]] = for {
+      database <- databaseReady
+      httpEndpoint <- httpEndpointReady
+      messageListener <- messageListenerReady
+    } yield {
+      messageListener
+    }
+
+    ready.onComplete {
+      case Success(consumer) => {
+        consumer.handler(message => ???)
+        startPromise.success()
+      }
+      case Failure(cause) => {
+        startPromise.failure(cause)
+      }
+    }
 
   }
 
